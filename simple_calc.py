a39a7fd43cbfdca3f68efd5274ed04ddc8ff84a9
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-02-07 09:42:26 +0100

    Implement basic calculator class and basic operations

---
+++ simple_calc.py
@@ -0,0 +1,9 @@
+class Calc():
+    def __init__(self):
+        result = 0
+
+    def add(self, addend1, addend2):
+        return addend1 + addend2
+
+    def subtract(self, operand1, operand2):
+        return operand1 - operand2
