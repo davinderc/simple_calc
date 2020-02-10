878b827c1544004d1c797531810801f2ec0ededc
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-02-10 09:52:27 +0100

    Define contracts to ensure operands are between 0 and 9

--- simple_calc.py
+++ simple_calc.py
@@ -1,9 +1,12 @@
+from contracts import contract
+
 class Calc():
     def __init__(self):
         result = 0
-
+    @contract(addend1='int,>=0,<10', addend2='int,>=0,<10', returns='int')
     def add(self, addend1, addend2):
         return addend1 + addend2
 
+    @contract(operand1='int,>=0,<10', operand2='int,>=0,<10', returns='int')
     def subtract(self, operand1, operand2):
         return operand1 - operand2
