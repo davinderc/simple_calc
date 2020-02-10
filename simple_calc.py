922ff582ce0dd5889384a27c1d2b9c51eb3f523d
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-02-10 11:05:59 +0100

    Rename parameters in add

--- simple_calc.py
+++ simple_calc.py
@@ -3,10 +3,22 @@ from contracts import contract
 class Calc():
     def __init__(self):
         result = 0
-    @contract(addend1='int,>=0,<10', addend2='int,>=0,<10', returns='int')
-    def add(self, addend1, addend2):
-        return addend1 + addend2
+    @contract(operand1='int,>=0,<10', operand2='int,>=0,<10', returns='int')
+    def add(self, operand1, operand2):
+        """
+        Adds two integers between 0 and 9
+        :param operand1:
+        :param operand2:
+        :return:
+        """
+        return operand1 + operand2
 
     @contract(operand1='int,>=0,<10', operand2='int,>=0,<10', returns='int')
     def subtract(self, operand1, operand2):
+        """
+        Subtracts two integers between 0 and 9
+        :param operand1:
+        :param operand2:
+        :return:
+        """
         return operand1 - operand2
