ece883f27ffc63566f8a512aaf9ae4bcea31b8d0
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-02-11 11:45:47 +0100

    Organize module for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple-calc-davinderc",
+    version="0.0.1",
+    author="Davinder C",
+    author_email="davinder.sc1@gmail.com",
+    description="A simple calculator to developed to practice test-driven development.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url="https://github.com/davinderc/simple_calc.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
